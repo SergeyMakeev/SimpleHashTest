d49fec435d26a0fd2f9ef6e9f7389863ac1d603c
Sergey Makeev <sergeymakeev@hotmail.com>
2022-04-21 18:50:07 -0700

    fix linker

--- main.cpp
+++ main.cpp
@@ -11,95 +11,71 @@
 
 #include "Luau/DenseHash.h"
 
+#define _PROFILEAPI_H_
 #define FOLLY_NO_CONFIG
 #include "folly/container/F14Map.h"
-#include "folly/lang/SafeAssert.h"
+#include "folly/lang/SafeAssert.cpp"
+#include "folly/lang/ToAscii.cpp"
+#include "folly/ScopeGuard.cpp"
+#include "folly/container/detail/F14Table.cpp"
 
-namespace folly
-{
-	namespace detail
-	{
-		template <>
-		void safe_assert_terminate<0>(safe_assert_arg const* arg, ...) noexcept {
-			__debugbreak();
-		}
-
-		template <>
-		void safe_assert_terminate<1>(safe_assert_arg const* arg, ...) noexcept {
-			__debugbreak();
-		}
-	}
-
-	namespace f14 {
-		namespace detail {
-			EmptyTagVectorType kEmptyTagVector = {};
-		}
-	}
-
-}
 
 #include "boost/unordered/unordered_map.hpp"
 
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/internal/raw_hash_set.cc"
+#include "absl/hash/internal/hash.cc"
+#include "absl/hash/internal/city.cc"
+#include "absl/hash/internal/low_level_hash.cc"
 
-namespace absl {
-	namespace container_internal {
-		// A single block of empty control bytes for tables without any slots allocated.
-		// This enables removing a branch in the hot path of find().
-		alignas(16) ABSL_CONST_INIT ABSL_DLL const ctrl_t kEmptyGroup[16] = {
-			ctrl_t::kSentinel, ctrl_t::kEmpty, ctrl_t::kEmpty, ctrl_t::kEmpty,
-			ctrl_t::kEmpty,    ctrl_t::kEmpty, ctrl_t::kEmpty, ctrl_t::kEmpty,
-			ctrl_t::kEmpty,    ctrl_t::kEmpty, ctrl_t::kEmpty, ctrl_t::kEmpty,
-			ctrl_t::kEmpty,    ctrl_t::kEmpty, ctrl_t::kEmpty, ctrl_t::kEmpty };
-	}
-}
+
 
 
 #include "llvm/ADT/DenseMap.h"
-
-#include "llvm/Support/MemAlloc.h"
-#include <new>
-
-// These are out of line to have __cpp_aligned_new not affect ABI.
-
-LLVM_ATTRIBUTE_RETURNS_NONNULL LLVM_ATTRIBUTE_RETURNS_NOALIAS void*
-llvm::allocate_buffer(size_t Size, size_t Alignment) {
-	return ::operator new(Size
-#ifdef __cpp_aligned_new
-		,
-		std::align_val_t(Alignment)
-#endif
-		);
-}
-
-void llvm::deallocate_buffer(void* Ptr, size_t Size, size_t Alignment) {
-	::operator delete(Ptr
-#ifdef __cpp_sized_deallocation
-		,
-		Size
-#endif
-#ifdef __cpp_aligned_new
-		,
-		std::align_val_t(Alignment)
-#endif
-		);
-}
+#include "Support/MemAlloc.cpp"
 
 
 void test()
 {
 	Excalibur::HashTable<int, int> test01;
+	test01.emplace(1, 2);
+
 	ska::flat_hash_map<int, int> test02;
+	test02.emplace(1, 2);
+
 	ska::unordered_map<int, int> test03;
+	test03.emplace(1, 2);
+
 	tsl::robin_map<int, int> test04;
+	test04.emplace(1, 2);
+
 	Luau::DenseHashMap<int, int> test05(INT_MIN);
+	test05[1] = 2;
+
 	folly::F14ValueMap<int, int> test06;
+	test06.emplace(1, 2);
+
 	boost::unordered_map<int, int> test07;
+	test07.emplace(1, 2);
+
 	absl::flat_hash_map<int, int> test08;
+	test08.emplace(1, 2);
+
 	llvm::DenseMap<int, int> test09;
+	test09.try_emplace(1, 2);
 }
 
+
+struct PreMain
+{
+	PreMain()
+	{
+		test();
+	}
+};
+
+PreMain preMain;
 
 
 UBENCH_MAIN()
