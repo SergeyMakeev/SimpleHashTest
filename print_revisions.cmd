4bf313b649f930bcd27e08d29f92fd95b33a09f9
Sergey Makeev <sergeymakeev@hotmail.com>
2024-01-22 09:17:15 -0800

    add script to print all external revisions

---
+++ print_revisions.cmd
@@ -0,0 +1,40 @@
+cd ./extern/abseil-cpp
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/Boost/unordered
+git rev-parse HEAD
+cd ../..
+
+
+cd ./extern/ExcaliburHash
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/flat_hash_map
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/folly
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/llvm-project
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/luau
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/parallel-hashmap
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/robin-map
+git rev-parse HEAD
+cd ../..
+
+cd ./extern/sparsehash
+git rev-parse HEAD
+cd ../..
